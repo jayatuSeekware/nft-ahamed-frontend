9335488a220394b348a05cff5104dc5485bb466a
shikhars371 <shikhars371@gmail.com>
2021-05-13 18:45:12 +0530

    .

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -64,43 +64,42 @@ class Signin extends React.Component {
     render() {
         return (
             <>
-                <div className="user-screen-area">
-                    <div className="user-screen-box">
-                        <div className="user-screen-item screen-from">
-                            <div className="user-form">
-                                    <div className="nft-input-box validate-input" dataa-validate="Name is required">
-                                        <span className="label-nft-input">User Name</span>
-                                        <input className="nft-input" type="text" name="login" placeholder="Enter email" value={this.state.email} onChange={this.handleEmail}></input>
-                                        <span className="focus-nft-input"></span>
-                                    </div>
+        <section className="user-screen-area">
+        <div className="user-screen-box">
+            <div className="user-screen-item screen-form">
+               <div className="user-form">
+                   <form>
+                     <div className="nft-input-box validate-input" data-validate="Email is required">
+                        <span className="label-nft-input">Email</span>
+                        <input className="nft-input" type="text" name="name" placeholder="Enter Email" value={this.state.email} onChange={this.handleEmail}/>
+                        <span className="focus-nft-input"></span>
+                     </div>
+                     <div className="nft-input-box validate-input" data-validate="Name is required">
+                        <span className="label-nft-input">Password</span>
+                        <input className="nft-input" type="password" name="name" placeholder="******" value={this.state.password} onChange={this.handlePassword}/>
+                        <span className="focus-nft-input"></span>
+                     </div>
+                     <div className="nft-links-btn">
+                        <a href="/">Forgot My Password ?</a>
+                     </div>
+                     <div className="nft-links-btn">
+                        <a onClick={this.signin} className="theme-btn">Log In</a>
+                        <p className="theme-description text-center">Don’t have an account? <a href="/Signup">Sign Up</a></p>
+                     </div>
+                     </form>
+               </div>
 
-                                    <div className="nft-input-box validate-input" data-validate="Name is required">
-                                        <span className="label-nft-input">Password</span>
-                                        <input type={this.state.hidden ? 'password' : 'text'} name="login" placeholder="Enter password" value={this.state.password} onChange={this.handlePassword}></input>
-                                        <span className="focus-nft-input"></span>
-                                        
-                                    </div>
-
-                                    <div className="nft-links-btn">
-                                        <a href="/Signup">Forgot My Password ?</a>
-                                    </div>
-
-                                    <div className="nft-links-btn">
-                                        <input type="submit" className="theme-btn" value="Log In" onClick={this.signin}></input>
-                                        <p className="theme-description text-center"> Don't have an account? <a className="underlineHover" href="/Signup">Signup</a></p>
-                                    </div>
-                            </div>
-                            <div className="user-copyright">
-                                <p className="theme-description">Copyright 2021 <a href="index.html">NFT Marketplace</a> All Rights Reserved.</p>
-                            </div>
-                        </div>       
-    
-                        <div className="user-screen-item screen-bg-image text-center">
-   
-                        </div> 
-                    </div>
-                </div>
-
+               <div className="user-copyright">
+                  <p className="theme-description">Copyright 2021 <a href="/">NFT Marketplace</a> All Rights Reserved.</p>
+               </div>
+            </div>
+            <div className="user-screen-item screen-bg-image text-center">
+               <div className="user-logo">
+                  <a href="index.html"><img src={LoGO}/></a>
+               </div>
+            </div>
+         </div>
+         </section>
             </>
         )
     }
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     Contract_address : "0xDf68F93C6916Cd66c6C928F3fA1c6A1eEc5AE35a",
-    Platform_address : "0xc609914f597d5039AeA4698c84a06cdAc5c8Feb2"
+    Platform_address : "0xdC8B608b4031e0F84a3Cd2A1fFb7F209E04F4514"
 }
                         
