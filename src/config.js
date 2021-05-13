aa7fe085d627e2e4f7ed3feee656fdfbcd8c0801
shikhars371 <shikhars371@gmail.com>
2021-05-13 18:52:08 +0530

    .

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    Contract_address : "0xDf68F93C6916Cd66c6C928F3fA1c6A1eEc5AE35a",
+    Contract_address : "0x4A1197Da06F8629d912dEe6A0Fa3e6DDa67D0eD9",
     Platform_address : "0xdC8B608b4031e0F84a3Cd2A1fFb7F209E04F4514"
 }
                         
